6972726f715941f43cc53a300f48ac5c4de31f09
SuperRonanCraft <SuperRonanCraft@gmail.com>
2021-04-27 14:54:42 -0400

    updated ignore file + updated shade plugin for java version

--- src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
+++ src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
@@ -1,10 +1,7 @@
 package me.Dragonhuntrrr.DragonDiscordSync.Discord;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.entities.Activity;
-import org.bukkit.Bukkit;
 
 import javax.security.auth.login.LoginException;
 
