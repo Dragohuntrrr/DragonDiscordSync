55b8e81f9f874d748f29b29e309256addfd41a1a
Dragohuntrrr <78875905+Dragohuntrrr@users.noreply.github.com>
2021-05-02 12:43:47 +0200

    Added util package for files + added bStats

--- src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
+++ src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
@@ -1,7 +1,9 @@
 package me.Dragonhuntrrr.DragonDiscordSync.Discord;
 
+import me.Dragonhuntrrr.DragonDiscordSync.DragonDiscordSync;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import org.bukkit.Bukkit;
 
 import javax.security.auth.login.LoginException;
 
@@ -10,12 +12,12 @@ public class DiscordBot {
     public static void startBot(String token) {
         JDABuilder builder = JDABuilder.createDefault(token);
 
-        builder.setActivity(Activity.listening("to Dragons instructions"));
+        builder.setActivity(Activity.of(Activity.ActivityType.CUSTOM_STATUS, "Wag wan!"));
 
         try {
             builder.build();
         } catch (LoginException e) {
-            e.printStackTrace();
+            Bukkit.getLogger().severe("Invalid Bot-token!");
         }
     }
 
--- src/main/java/me/Dragonhuntrrr/DragonDiscordSync/DragonDiscordSync.java
+++ src/main/java/me/Dragonhuntrrr/DragonDiscordSync/DragonDiscordSync.java
@@ -1,17 +1,29 @@
 package me.Dragonhuntrrr.DragonDiscordSync;
 
 import me.Dragonhuntrrr.DragonDiscordSync.Discord.DiscordBot;
+import me.Dragonhuntrrr.DragonDiscordSync.Util.File.FileBasics;
+import me.Dragonhuntrrr.DragonDiscordSync.Util.bStats.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DragonDiscordSync extends JavaPlugin {
 
     private static DragonDiscordSync instance;
+    FileBasics configs = new FileBasics();
 
     @Override
     public void onEnable() {
+
         instance = this;
+
+        Metrics metrics = new Metrics(this, 11215);
+
+        configs.load();
+        FileBasics.FILETYPE.CONFIG.setValue("Plugin-Version", this.getDescription().getVersion());
+        FileBasics.FILETYPE.CONFIG.save();
+
+        DiscordBot.startBot(FileBasics.FILETYPE.CONFIG.getString("Bot-token"));
+
         System.out.println("DragonDiscordSync: Plugin enabled!");
-        DiscordBot.startBot("ODMzODI5OTU4MTc4NDM5MjI4.YH4CqQ.7RSDUZ0E3vX0MoyoDfuBhjZpbDc");
     }
 
     public static DragonDiscordSync getInstance() {
