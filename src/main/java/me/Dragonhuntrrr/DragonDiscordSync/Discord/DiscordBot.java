8af66d27a5e914f55ef6bcf487413ba9a42afd6e
Dragohuntrrr <78875905+Dragohuntrrr@users.noreply.github.com>
2021-05-02 13:05:21 +0200

    Added support for user defined activities

--- src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
+++ src/main/java/me/Dragonhuntrrr/DragonDiscordSync/Discord/DiscordBot.java
@@ -1,6 +1,7 @@
 package me.Dragonhuntrrr.DragonDiscordSync.Discord;
 
 import me.Dragonhuntrrr.DragonDiscordSync.DragonDiscordSync;
+import me.Dragonhuntrrr.DragonDiscordSync.Util.File.FileBasics;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import org.bukkit.Bukkit;
@@ -12,12 +13,27 @@ public class DiscordBot {
     public static void startBot(String token) {
         JDABuilder builder = JDABuilder.createDefault(token);
 
-        builder.setActivity(Activity.of(Activity.ActivityType.CUSTOM_STATUS, "Wag wan!"));
+        if (FileBasics.FILETYPE.CONFIG.getString("Bot-activity").equalsIgnoreCase("Playing")) {
+            builder.setActivity(Activity.of(Activity.ActivityType.DEFAULT,
+                    FileBasics.FILETYPE.CONFIG.getString("Bot-activity-text")));
+        } else if (FileBasics.FILETYPE.CONFIG.getString("Bot-activity").equalsIgnoreCase("Listening")) {
+            builder.setActivity(Activity.of(Activity.ActivityType.LISTENING,
+                    FileBasics.FILETYPE.CONFIG.getString("Bot-activity-text")));
+        } else if (FileBasics.FILETYPE.CONFIG.getString("Bot-activity").equalsIgnoreCase("Watching")) {
+            builder.setActivity(Activity.of(Activity.ActivityType.WATCHING,
+                    FileBasics.FILETYPE.CONFIG.getString("Bot-activity-text")));
+        } else if (FileBasics.FILETYPE.CONFIG.getString("Bot-activity").equalsIgnoreCase("Custom")) {
+            builder.setActivity(Activity.of(Activity.ActivityType.CUSTOM_STATUS,
+                    FileBasics.FILETYPE.CONFIG.getString("Bot-activity-text")));
+        } else if (FileBasics.FILETYPE.CONFIG.getString("Bot-activity").equalsIgnoreCase("Streaming")) {
+            builder.setActivity(Activity.of(Activity.ActivityType.CUSTOM_STATUS,
+                    FileBasics.FILETYPE.CONFIG.getString("Bot-activity-text"), FileBasics.FILETYPE.CONFIG.getString("Bot-activity-url")));
+        }
 
         try {
             builder.build();
         } catch (LoginException e) {
-            Bukkit.getLogger().severe("Invalid Bot-token!");
+            Bukkit.getLogger().severe("[DragonDiscordSync]: Invalid Bot-token!");
         }
     }
 
